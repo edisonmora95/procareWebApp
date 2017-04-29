f288e7f3acf7cc4d93b8d7ab9f700c005e11915b
edanmora95 <edison_andre_9@hotmail.com>
2017-04-28 19:56:21 -0500

    Modificaciones a ingresar procariano
    Mejorado el css.
    Creados los selects de manera dinámica.

--- procareApp/public/js/procariano/ingresarProcarianoScript.js
+++ procareApp/public/js/procariano/ingresarProcarianoScript.js
@@ -2,15 +2,21 @@
 var main = new Vue({
 	el: '#main',
 	mounted: function(){
-		 $('.datepicker').pickadate({
-		    selectMonths: true, // Creates a dropdown to control month
-		    selectYears: 100 // Creates a dropdown of 15 years to control year
-		  });
-		 $('#select-tipo-procariano').material_select();
-		 $('#select-genero').material_select();
-		 $('#select-grupo-formacion').material_select();
-		 $('#select-grupo-caminante').material_select();
-		  $(".button-collapse").sideNav();
+		
+		$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 100 // Creates a dropdown of 15 years to control year
+		});
+		$('#select-tipo-procariano').material_select();
+		$('#select-genero').material_select();
+		$('#select-grupo-formacion').material_select();
+		$('#select-grupo-caminante').material_select();
+		$(".button-collapse").sideNav();
+		//this.crearSelectGrupoFormacion();
+		this.crearSelectGrupo('select-grupo-formacion', this.grupoFormacionEscogido, 'div-select-grupo-formacion', this.gruposFormacion);
+		this.crearSelectGrupo('select-grupo-caminantes', this.grupoCaminantesEscogido, 'div-select-grupo-caminantes', this.gruposCaminantes);
+		this.crearSelectGrupo('select-grupo-pescadores', this.grupoPescadoresEscogido, 'div-select-grupo-pescadores', this.gruposPescadores);
+		$('.modal').modal();
 	},
 	data: {
 		usuario: '',
@@ -34,10 +40,61 @@ var main = new Vue({
 			fechaOrdenacion: ''
 		},
 		src: '',
-		gruposCaminantes: [],
-		gruposFormacion: [],
-		gruposPescadores: [],
-		gruposMayores: []
+		gruposCaminantes: [
+			{
+				nombre: 'Grupo del Chino',
+				id: '1'
+			},
+			{
+				nombre: 'Grupo de Caminantes Viejos',
+				id: '2'
+			}
+		],
+		grupoCaminantesEscogido: {
+			nombre: '',
+			id: ''
+		},
+		gruposFormacion: [
+			{
+				nombre: 'Grupo de Luis',
+				id: '1'
+			},
+			{
+				nombre: 'Grupo de Mario',
+				id: '2'
+			},
+			{
+				nombre: 'Grupo de Fernando',
+				id: '3'
+			}
+		],
+		grupoFormacionEscogido: {
+			nombre: '',
+			id: ''
+		},
+		gruposPescadores: [
+			{
+				nombre: 'Grupo de Pescadores 1',
+				id: '1'
+			},
+			{
+				nombre: 'Grupo de Pescadores 2',
+				id: '2'
+			},
+			{
+				nombre: 'Grupo de Pescadores 3',
+				id: '3'
+			}
+		],
+		grupoPescadoresEscogido: {
+			nombre: '',
+			id: ''
+		},
+		gruposMayores: [],
+		grupoMayoresEscogido: {
+			nombre: '',
+			id: ''
+		}
 	},
 	methods: {
 		prueba: function(){
@@ -50,20 +107,62 @@ var main = new Vue({
 			if(usuario=='personal'){
 				
 			}
+		},
+		crearSelectGrupo: function(idSelect, grupoEscogido, idDivSelect, grupos){
+			/*
+				Parámetros:
+					idSelect -> id del elemento select que se va a crear en esta función para contener a los grupos deseados. Ejemplo: select-grupo-formacion
+					grupoEscogido ->  Elemento de data con el cual se hará el 2 way data binding. Almacenará el grupo escogido del select
+					idDivSelect -> id del div que contendrá al elemento select que se va a crear
+					grupos -> Los grupos que se van a mostrar en el select
+			*/
+			//Todos los parametros de id vienen sin el #
+			var self = this;
+			var select = $('<select>').attr({"id":idSelect});
+			var optionSelectedAux = '#' + idSelect + ' option:selected';
+			select.change(function(){
+				grupoEscogido.id = $(optionSelectedAux).val();
+				grupoEscogido.nombre = $(optionSelectedAux).text();
+				self.procariano.grupo = $(optionSelectedAux).text();
+			});
+			var idDivSelectAux = '#' + idDivSelect;
+			var divSelect = $(idDivSelectAux);
+			self.crearSelectOptions(select, grupos, divSelect);
+			divSelect.append(select);
+			select.material_select();
+		},	
+		crearSelectOptions: function(select, grupos, divSelect){
+			/*
+				Parámetros:
+					select -> elemento select creado en la función crearSelectGrupo que mostrará a los grupos deseados
+					grupos -> los grupos que se mostrarán como opciones dentro del select
+					divSelect -> elemento div que contendrá al select
+			*/
+			var self = this;
+			var optionDisabled = $('<option>').val("").text("");
+			select.append(optionDisabled);
+			$.each(grupos, function(index, grupo){
+				var option = $('<option>').val(grupo.id).text(grupo.nombre);
+				select.append(option);
+			});
+			divSelect.append(select)
+		},
+		crearProcariano: function(){
+			$('#modalProcarianoCreado').modal('open');
 		}
 	}
 });
 
 $('#select-tipo-procariano').change(function(){
-	//console.log('Val: ' + $('#select-tipo-procariano').val())
-	//console.log('Text: ' + $('#select-tipo-procariano').text())
 	var tipoProcariano = $('#select-tipo-procariano option:selected').text()
 	main.$data.procariano.tipo = tipoProcariano;
 	console.log(tipoProcariano)
 	console.log(main.$data.procariano)
-	if(tipoProcariano=='Chico de Formación'){
-		$('#select-grupo-formacion').material_select();
-	}
+});
+
+$('#select-genero').change(function(){
+	var generoProcariano = $('#select-genero option:selected').text();
+	main.$data.procariano.genero = generoProcariano;
 });
 
 $('#fecha-nacimiento').change(function(){
