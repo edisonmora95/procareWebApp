c140c3c310d9e368d44894a4790359b0be12629c
edanmora95 <edison_andre_9@hotmail.com>
2017-05-20 19:39:41 -0500

    Ficha de procariano

--- public/js/procariano/verProcarianoScript.js
+++ public/js/procariano/verProcarianoScript.js
@@ -2,6 +2,8 @@ var app = new Vue({
 	el: '#app',
 	mounted: function(){
 		$(".button-collapse").sideNav();
+		$('.tooltipped').tooltip({delay: 50});
+		this.formarNavbar();
 	},
 	data: {
 		procariano:{
@@ -25,6 +27,64 @@ var app = new Vue({
 		}
 	},
 	methods: {
-
+		formarNavbar: function(){
+			/*
+				@Descripción: Esta función crea el navbar dependiendo tel tipo de usuario que está loggeado.
+				@Autor: @edisonmora95
+				@FechaCreacion: 20-05-2017
+			*/
+			var usuario = 'personal';
+			if(usuario === 'personal'){
+				this.crearDropdownPA();
+				this.crearDropdownPF();
+			}
+		},
+		crearDropdownPA: function(){
+			/*
+				@Descripción: Esta función crea las pestañas del dropdown de Procare Acción del navbar.
+				@Autor: @edisonmora95
+				@FechaCreacion: 20-05-2017
+			*/
+			var liAsistencias = $('<li>');
+			var aAsistencias = $('<a>').html('Asistencias');
+			liAsistencias.append(aAsistencias);
+			$('#ulProcareAccion').append(liAsistencias);
+			var liParalelos = $('<li>')
+			var aParalelos = $('<a>').html('Paralelos');
+			liParalelos.append(aParalelos);
+			$('#ulProcareAccion').append(liParalelos);
+			var liNinos = $('<li>');
+			var aNinos = $('<a>').html('Niños');
+			liNinos.append(aNinos);
+			$('#ulProcareAccion').append(liNinos);
+		},
+		crearDropdownPF: function(){
+			/*
+				@Descripción: Esta función crea las pestañas del dropdown de Procare Formación del navbar.
+				@Autor: @edisonmora95
+				@FechaCreacion: 20-05-2017
+			*/
+			var liAsistencias = $('<li>');
+			var aAsistencias = $('<a>').html('Asistencias');
+			liAsistencias.append(aAsistencias);
+			$('#ulProcareFormacion').append(liAsistencias);
+			var liGrupos = $('<li>')
+			var aGrupos = $('<a>').html('Grupos');
+			liGrupos.append(aGrupos);
+			$('#ulProcareFormacion').append(liGrupos);
+			var liProcarianos = $('<li>');
+			var aProcarianos = $('<a>').html('Procarianos');
+			liProcarianos.append(aProcarianos);
+			$('#ulProcareFormacion').append(liProcarianos);
+		},
+		eliminar: function(){
+			/*
+				@Autor: @edisonmora95
+				@FechaCreación: 20-05-2017
+			*/
+			//Llamada a la api para eliminar al procariano
+			this.procariano.estado = 'inactivo';
+			Materialize.toast('Procariano cambiado a estado inactivo', 2000, 'rounded')
+		}
 	}
 });
