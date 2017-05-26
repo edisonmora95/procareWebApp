ad1ef82dafc35a16985ebe7b14571fdf246fc5ec
edanmora95 <edison_andre_9@hotmail.com>
2017-05-25 19:53:45 -0500

    Automatización de babel y vueify
    npm run build cada vez que se hagan cambios a algún .js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+var browserify = require('gulp-browserify');
+var runSequence = require('run-sequence');
 
 gulp.task('default', ['browser-sync'], function(){
 
@@ -27,4 +30,32 @@ gulp.task('nodemon', function(cb){
 			started = true;
 		}
 	})
-})
+});
+
+gulp.task('babel', function(){
+	console.log('Convirtiendo a es5');
+	var dist = './public/dist/';
+	return gulp.src('./public/js/*/*')
+				.pipe(babel({
+					presets: ['es2015']
+				}))
+				.pipe(gulp.dest(dist));
+});
+
+gulp.task('vueify', function(){
+	console.log('Vueify');
+	//var src = './public/js/grupo/crearGrupoScript.js'
+	//var build = './build/grupo/bundle.js'
+	var src = './public/dist/**/*.js'
+	var build = './public/build/'
+	gulp.src(src, { read: false })
+			.pipe(browserify({
+				//debug: false,
+				transform: ['vueify'],
+			}))
+			.pipe(gulp.dest(build));
+});
+
+gulp.task('build', function(){
+	runSequence('babel', 'vueify');
+});
---
+++ public/templates/navbar.vue
@@ -0,0 +1,46 @@
+<template>
+	<nav>
+		<a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+		<a href="" class="brand-logo">Procare</a>
+		<ul id="slide-out" class="side-nav">
+	    <li>
+	    	<div class="userView">
+		      
+		    </div>
+	    </li>
+	    <li class="no-padding">
+	    	<ul class="collapsible collapsible-accordion">
+	    		<li>
+	    			<a class="collapsible-header waves-effect waves-teal">Procare Formación<i class="material-icons right">arrow_drop_down</i></a>
+	    			<div class="collapsible-body">
+	            <ul id="ulProcareFormacion">
+	            </ul>
+	          </div>
+	    		</li>
+	    	</ul>
+	    </li>
+	    <li class="no-padding">
+	    	<ul class="collapsible collapsible-accordion">
+	    		<li>
+	    			<a class="collapsible-header waves-effect waves-teal">Procare Acción<i class="material-icons right">arrow_drop_down</i></a>
+	    			<div class="collapsible-body">
+	            <ul id="ulProcareAccion">
+	            </ul>
+	          </div>
+	    		</li>
+	    	</ul>
+	    </li>
+	    <li><a href="#">Salir</a></li>
+	  </ul>
+	</nav>
+</template>
+
+<script>
+	module.exports = {
+		data: function() {
+			return{
+				greeting: 'Hello'
+			}
+		}
+	}
+</script>
