71972d9bdfdac46aff8a575a1d92d499f3849121
josealcivar <jose_alcivargarcia@hotmail.com>
2017-06-21 16:12:56 -0500

    sprint 1

---
+++ models/benefactor.js
@@ -0,0 +1,62 @@
+/*
+
+@Descripcion: Modelo de Benefactor
+@Autor: jose alcivar
+@FechaCreacion: 21/06/2017
+@UltimaFechaModificacion: 21/06/2017 @josealcivar
+
+
+*/
+
+
+var bcrypt = require('bcryptjs');
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Benefactor = sequelize.define('Benefactor', {
+    valor_contribucion: {
+      type : DataTypes.DECIMAL(10,2),
+      allowNull : false
+    },
+    dia_cobro: {
+      type : DataTypes.INTEGER(2),
+      allowNull : false
+    },
+    tarjeta_credito: {
+      type : DataTypes.STRING,
+      allowNull : true
+    },
+    tipo_donacion: {
+      type : DataTypes.STRING,
+      allowNull : true
+    },
+    estado: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      /*validate : {
+        isIn : ['activo', 'inactivo' ]
+      }*/
+    },
+    nombre_gestor: {
+      type : DataTypes.STRING,
+      allowNull : true
+    },
+
+    relacion: {
+      type : DataTypes.STRING,
+      allowNull : true
+    },
+    observacion: {
+      type : DataTypes.STRING,
+      allowNull : true
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        //Benefactor.belongsTo(models.Persona)
+        // associations can be defined here
+      }
+    }
+
+  });
+  return Benefactor;
+};
