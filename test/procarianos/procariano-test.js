fe819695fb37dd8cdb124a11a00e95fe2746d890
edanmora95 <edison_andre_9@hotmail.com>
2017-07-30 22:09:46 -0500

    Tests

--- test/procarianos/procariano-test.js
+++ test/procarianos/procariano-test.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 
 
 describe('Procarianos', function() {
-	 it('Prueba donde cedula es null, nombres y apellidos estan presentes', function(done) {
+	 /*it('Prueba donde cedula es null, nombres y apellidos estan presentes', function(done) {
 	  chai.request(server)
 	    .post('/api/procarianos/')
 	    .send({ 'apellidos': 'viteri cuenca' , 'nombres': 'jose antonio'})
@@ -26,8 +26,8 @@ describe('Procarianos', function() {
 	      //res.body.SUCCESS.lastName.should.equal('Script');
 	      done();
 	    });
-	});
-	  it('Prueba donde nombres es null, cedula y apellidos estan presentes' , function(done) {
+		});*/
+	 /*it('Prueba donde nombres es null, cedula y apellidos estan presentes' , function(done) {
 	  chai.request(server)
 	    .post('/api/procarianos/')
 	    .send({ 'apellidos': 'viteri cuenca' , 'cedula': '0987654321'})
@@ -35,19 +35,19 @@ describe('Procarianos', function() {
 	      res.should.have.status(200);
 	      res.should.be.json;
 	      res.body.should.be.a('object');
-	      /*
+	      
 	      res.body.persona.should.have.property('apellidos');
 	      res.body.persona.should.have.property('nombres');
 	      res.body.persona.should.have.property('cedula');
-	      assert.equal(req.body.persona.cedula.length, '10' , 'es igual')*/
+	      assert.equal(req.body.persona.cedula.length, '10' , 'es igual')
 	      res.body.mensaje.should.equal('No se pudo crear esta persona');
 	      res.body.status.should.equal(false);
 	      //res.body.SUCCESS.name.should.equal('Java');
 	      //res.body.SUCCESS.lastName.should.equal('Script');
 	      done();
 	    });
-	 });
-	  it('Prueba donde apellidos es null, cedula y nombres estan presentes' , function(done) {
+	 });*/
+	 /*it('Prueba donde apellidos es null, cedula y nombres estan presentes' , function(done) {
 	  chai.request(server)
 	    .post('/api/procarianos/')
 	    .send({ 'cedula': '0987654321' , 'nombres': 'jose antonio'})
@@ -56,11 +56,11 @@ describe('Procarianos', function() {
 	      res.should.be.json;
 	      res.body.should.be.a('object');
 	      //console.log(res.body);
-	      	      /*
+	      	      
 	      res.body.persona.should.have.property('apellidos');
 	      res.body.persona.should.have.property('nombres');
 	      res.body.persona.should.have.property('cedula');
-	      assert.equal(req.body.persona.cedula.length, '10' , 'es igual')*/
+	      assert.equal(req.body.persona.cedula.length, '10' , 'es igual')
 	      res.body.mensaje.should.equal('No se pudo crear esta persona');
 	      res.body.status.should.equal(false);
 
@@ -69,8 +69,8 @@ describe('Procarianos', function() {
 	      //res.body.SUCCESS.lastName.should.equal('Script');
 	      done();
 	    });
-	 });
-	  it('Prueba donde cedula , nombres y apellidos son diferentes de null y genero es masculino', function(done) {
+	 });*/
+	 /*it('Prueba donde cedula , nombres y apellidos son diferentes de null y genero es masculino', function(done) {
 	  chai.request(server)
 	    .post('/api/procarianos/')
 	    .send({ 'apellidos': 'viteri cuenca' , 'nombres': 'jose antonio' , 'cedula' : '0987659624' , 'genero' : 'masculino'})
@@ -88,8 +88,8 @@ describe('Procarianos', function() {
 	      //res.body.SUCCESS.lastName.should.equal('Script');
 	      done();
 	    });
-	 });
-	  it('Prueba donde cedula tiene longitud igual de 10, y nombres y apellidos son diferentes de null y genero es diferente de masculino y femenino', function(done) {
+	 });*/
+	 /*it('Prueba donde cedula tiene longitud igual de 10, y nombres y apellidos son diferentes de null y genero es diferente de masculino y femenino', function(done) {
 	  chai.request(server)
 	    .post('/api/procarianos/')
 	    .send({ 'apellidos': 'viteri cuenca' , 'nombres': 'jose antonio' , 'cedula' : '098765432109', 'genero' : 'sfdf'})
@@ -99,16 +99,16 @@ describe('Procarianos', function() {
 	      res.body.should.be.a('object');
 	      res.body.mensaje.should.equal('No se pudo crear esta persona');
 	      res.body.status.should.equal(false);
-	      /*
+	      
 	      res.body.persona.should.have.property('apellidos');
 	      res.body.persona.should.have.property('nombres');
 	      res.body.persona.should.have.property('cedula');
 	      assert.equal(res.body.persona.cedula.length, '10' , 'es igual')
-	      */
+	      
 	      //res.body.SUCCESS.name.should.equal('Java');
 	      //res.body.SUCCESS.lastName.should.equal('Script');
 	      done();
 	    });
-	  });
-	});
+	  });*/
+});
 
